629bb1144a215408442e982541b64239fefa68aa
Mark Shust <mark@shust.com>
2021-03-01 12:27:36 -0500

    Create an admin controller

---
+++ Controller/Adminhtml/Faq/Index.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+namespace Macademy\Minerva\Controller\Adminhtml\Faq;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action implements HttpGetActionInterface
+{
+    const ADMIN_RESOURCE = 'Macademy_Minerva::faq';
+
+    /** @var PageFactory */
+    protected $pageFactory;
+
+    /**
+     * Index constructor.
+     * @param Context $context
+     * @param PageFactory $pageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $pageFactory
+    ) {
+        parent::__construct($context);
+        $this->pageFactory = $pageFactory;
+    }
+
+    /**
+     * @return Page
+     */
+    public function execute(): Page
+    {
+        return $this->pageFactory->create();
+    }
+}
