8dd2d8518fc4a5b09c72c1123e15ff6754a80f16
Mark Shust <mark@shust.com>
2021-03-11 14:46:19 -0500

    Transform the admin actions column into a dropdown

--- Ui/Component/Listing/Column/Actions.php
+++ Ui/Component/Listing/Column/Actions.php
@@ -49,6 +49,12 @@ class Actions extends Column
                     ]),
                     'label' => __('Edit'),
                 ],
+                'delete' => [
+                    'href' => $this->urlBuilder->getUrl('minerva/faq/delete', [
+                        'id' => $item['id'],
+                    ]),
+                    'label' => __('Delete'),
+                ],
             ];
         }
 
