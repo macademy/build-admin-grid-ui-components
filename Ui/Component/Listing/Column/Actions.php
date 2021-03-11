d7bd73e407a36a56afbc4810944f936f04604fdc
Mark Shust <mark@shust.com>
2021-03-11 14:37:42 -0500

    Create an admin grid actions column

---
+++ Ui/Component/Listing/Column/Actions.php
@@ -0,0 +1,57 @@
+<?php declare(strict_types=1);
+
+namespace Macademy\Minerva\Ui\Component\Listing\Column;
+
+use Magento\Framework\UrlInterface;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Ui\Component\Listing\Columns\Column;
+
+class Actions extends Column
+{
+    /** @var UrlInterface */
+    protected $urlBuilder;
+
+    /**
+     * Actions constructor.
+     * @param ContextInterface $context
+     * @param UiComponentFactory $uiComponentFactory
+     * @param UrlInterface $urlBuilder
+     * @param array $components
+     * @param array $data
+     */
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        UrlInterface $urlBuilder,
+        array $components = [],
+        array $data = []
+    ) {
+        $this->urlBuilder = $urlBuilder;
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    public function prepareDataSource(array $dataSource): array
+    {
+        if (!isset($dataSource['data']['items'])) {
+            return $dataSource;
+        }
+
+        foreach ($dataSource['data']['items'] as & $item) {
+            if (!isset($item['id'])) {
+                continue;
+            }
+
+            $item[$this->getData('name')] = [
+                'edit' => [
+                    'href' => $this->urlBuilder->getUrl('minerva/faq/edit', [
+                        'id' => $item['id'],
+                    ]),
+                    'label' => __('Edit'),
+                ],
+            ];
+        }
+
+        return $dataSource;
+    }
+}
