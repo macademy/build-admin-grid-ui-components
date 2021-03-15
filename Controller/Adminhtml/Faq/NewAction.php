b3fff60be257382d178efe99d19e5e1fc1ea2326
Mark Shust <mark@shust.com>
2021-03-15 16:00:20 -0400

    Create an admin controller for adding new records

---
+++ Controller/Adminhtml/Faq/NewAction.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+namespace Macademy\Minerva\Controller\Adminhtml\Faq;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
+
+class NewAction extends Action implements HttpGetActionInterface
+{
+    const ADMIN_RESOURCE = 'Macademy_Minerva::faq_save';
+
+    /** @var PageFactory */
+    protected $pageFactory;
+
+    /**
+     * Index constructor.
+     * @param Context $context
+     * @param PageFactory $pageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $pageFactory
+    ) {
+        parent::__construct($context);
+        $this->pageFactory = $pageFactory;
+    }
+
+    /**
+     * @return Page
+     */
+    public function execute(): Page
+    {
+        $page = $this->pageFactory->create();
+        $page->setActiveMenu('Macademy_Minerva::faq');
+        $page->getConfig()->getTitle()->prepend(__('New FAQ'));
+
+        return $page;
+    }
+}
